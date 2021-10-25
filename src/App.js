8bb79f1f94b23e0c604324bb0642dee2fe1e875a
Wafi benjeddou <wafibenjeddou@gmail.com>
2021-10-25 20:21:49 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React, { useState ,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 function App() {
+  const arraOfusers = [
+    { email: "siwar@gmail.com", password: "77777777777777" },
+  ];
   const [user, setUser] = useState({});
+  const [users, setUsers] = useState(arraOfusers);
   function handleChange(event) {
     setUser({ ...user, [event.target.id]: event.target.value });
+  
   }
   
- function onSubmit(event){
-   handleChange(event)
-   alert('it  works');
- }
-useEffect(()=>{
-  console.log('hello i am in work');
-},[])
+
+  function onSubmit(event) {
+    event.preventDefault();
+    handleChange(event);
+     alert("it  works"); 
+    setUsers([...users, user]);
+    setUser(" ")
+    console.log(user);
+  }
+  
+
+  useEffect(() => {
+    console.log("hello i am in work");
+  }, []);
 
   return (
     <div className="container">
       <form onSubmit={onSubmit}>
         <div className="form-group">
-          <label for="exampleInputEmail1">Email address</label>
+          <label for="
+          exampleInputEmail1">Email address</label>
           <input
+            value={user.email}
             onChange={handleChange}
             type="email"
             className="form-control"
@@ -33,6 +47,7 @@ useEffect(()=>{
         <div className="form-group">
           <label for="exampleInputPassword1">Password</label>
           <input
+          value={user.password}
             onChange={handleChange}
             type="password"
             className="form-control"
@@ -44,9 +59,6 @@ useEffect(()=>{
           Submit
         </button>
       </form>
-      <div>
-        <h1>hello</h1>
-      </div>
     </div>
   );
 }
